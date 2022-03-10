a6eda39ec47e00aacfe32b23b2755c43e505ede9
Leulme <kebedemichael9@gmail.com>
2022-03-10 23:05:16 +0300

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
-
 #include <stdio.h>
 
-/* This is 4-put.c*/
+/**
+ * main - the main function
+ * It prints the qouted sentence
+ * Description: this is a printing function
+ * Return: value of int 0
+ * @return int
+ */
 
 int main(void)
 {
 
-	char myStr[] = "Programming is like building a multilingual puzzle";
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
 
