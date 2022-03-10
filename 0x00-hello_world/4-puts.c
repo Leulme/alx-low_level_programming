7295d46fdfb9858e2113e18d833a790cfca8d1e2
Leulme <kebedemichael9@gmail.com>
2022-03-10 22:43:52 +0300

    Added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
-#<stdio.h>
 
-int main(){
+#include <stdio.h>
+
+/* This is 4-put.c*/
+
+int main(void)
+{
 
 	char myStr[50] = "Programming is like building a multilingual puzzle";
+
 	puts(myStr);
 
-	return 0;
+	return (0);
 
 }
