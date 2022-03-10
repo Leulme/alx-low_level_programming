4151faed32aa4291116b4705db2c7f5a033db8ce
Leulme <kebedemichael9@gmail.com>
2022-03-10 23:29:31 +0300

    Added 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - is the main function
+ * Description: it prints string
+ * Return: int value of 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
