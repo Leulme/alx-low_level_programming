faffcaa3962c1b9c283873ccf1ccd83a09c28d6d
Leulme <kebedemichael9@gmail.com>
2022-03-14 12:20:06 +0300

    Added 2-print_alpahbet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main -  is the main function
+ * Descitption: prints alphabets
+ * Return: int value of 0
+ */
+int main(void)
+{
+for (int i = 97; i < 123; i++)
+{
+putchar(i);
+}
+putchar(10);
+}
