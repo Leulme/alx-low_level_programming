a08ad49aa5387197f3f60ef8b5985bd1f86a829e
Leulme <kebedemichael9@gmail.com>
2022-03-14 13:20:47 +0300

    Added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+/**
+ * main -  is the main function
+ * Descitption: prints alphabets
+ * Return: int value of 0
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+putchar(10);
+}
+return (0);
+}
