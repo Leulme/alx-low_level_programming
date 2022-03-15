d0ebf5ebf52601c656fd74c769291f825fca0280
Leulme <kebedemichael9@gmail.com>
2022-03-15 11:26:01 +0300

    Added 1-alphaet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+
+/**
+ * print_alphabet - function that prints lower case alphabets
+ * Description: prints alphabets
+ * Return: returns void
+ */
+
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+}
