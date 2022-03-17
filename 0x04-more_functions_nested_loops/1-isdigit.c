9b73c71209813fdf61edbecbfc7f6a6593508619
Leulme <kebedemichael9@gmail.com>
2022-03-17 14:19:11 +0300

    Added the 2nd task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+# include "main.h"
+/**
+ * _isdigit - is a funciton
+ * @c: is a variable input
+ * Description: checks if c is upper case letter
+ * Return: integer value of 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
