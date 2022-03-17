63e1dc3adc98f03b87e4788054d3f84e1a9af055
Leulme <kebedemichael9@gmail.com>
2022-03-17 14:25:22 +0300

    Added the 3rd task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * mul - is a funciton
+ * @a: is a variable input
+ * @b: is another variavble input
+ * Description: multiply a and b
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
