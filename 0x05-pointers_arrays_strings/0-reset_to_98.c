c5258b8dfb5727aea130f5a854ef61b9e84769fe
Leulme <kebedemichael9@gmail.com>
2022-03-21 20:33:43 +0300

    Added the first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ * reset_to_98 - is a function
+ * @n: is a variable
+ * Description: is a function that sets value to 98
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
