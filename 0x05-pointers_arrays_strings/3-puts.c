e870c6ca8111f85eaee27ceeb7918d3c801f0dcd
Leulme <kebedemichael9@gmail.com>
2022-03-22 12:37:17 +0300

    Added the fouth task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * _puts - is a function
+ * @str: is a variable
+ * Description: write a string
+ * to stdout
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
