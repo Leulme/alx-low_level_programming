6bc985d2807cfb9146e9fa506edd53e8a145b4e8
Leulme <kebedemichael9@gmail.com>
2022-03-22 14:02:42 +0300

    Added the fifth task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * print_rev - is a function
+ * @s: is a variagle
+ * Description: prints string in reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char reversedstr[200];
+	char *reversed = reversedstr;
+	int countchar;
+
+	countchar = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		countchar++;
+	}
+
+
+	while (countchar >= 0)
+	{
+		s--;
+		*reversed = *s;
+		reversed++;
+		countchar--;
+	}
+
+	*reversed = '\0';
+
+	printf("%s\n", reversedstr);
+
+}
