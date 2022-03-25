ad40ec46d8d9f21cdf2ba9a9775bbde39e549f54
Leulme <kebedemichael9@gmail.com>
2022-03-25 10:26:11 +0300

    Added the fifth task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+# include <string.h>
+# include "main.h"
+/**
+ * reverse_array - is a function
+ * @a: is a pointer variable
+ * @n: is an integer variable
+ * Description: reverse array elements
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int j;
+int i;
+int temp;
+
+j = n-1;
+for (i = 0; i < n; i++)
+{
+if (i >= j)
+{
+break;
+}
+else{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+j--;
+}
+}
+}
