38d5213b04497fa3ff37fc293995bd80058d2c46
Leulme <kebedemichael9@gmail.com>
2022-03-25 09:56:30 +0300

    Added the fourth task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+# include <string.h>
+# include "main.h"
+/**
+ * _strcmp - is a function
+ * @s1: is a variable
+ * @s2: is a variable
+ * Description: compare two strings
+ * Return: integer value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
